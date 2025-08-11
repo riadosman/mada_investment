d4c90bf06b214f5080fc49699aff0549e62d2238
riad osman <riyados973@gmail.com>
2025-08-11 19:04:56 +0300

    add google tag manager

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Cairo } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 const cairoSans = Cairo({ subsets: ["latin"] });
 
@@ -13,12 +14,34 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body
-        className={`${cairoSans.variable} ${cairoSans.variable} antialiased`}
-      >
+      <head>
+        {/* كود GTM للتحميل غير المتزامن */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TLM8M5LM');
+            `,
+          }}
+        />
+      </head>
+      <body className={`${cairoSans.variable} antialiased`}>
         <Header />
         {children}
         <Footer />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TLM8M5LM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
