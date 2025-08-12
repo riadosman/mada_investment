a4ece0a076750febd65dfbe70845704d33c7514c
riad osman <riyados973@gmail.com>
2025-08-12 19:28:11 +0300

    add it to layout

--- app/layout.js
+++ app/layout.js
@@ -15,18 +15,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <head>
-        {/* كود GTM للتحميل غير المتزامن */}
         <Script
-          id="gtm-script"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11081777142"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-ads-gtag"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TLM8M5LM');
-            `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11081777142');
+          `,
           }}
         />
       </head>
@@ -34,14 +36,6 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TLM8M5LM"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
       </body>
     </html>
   );
